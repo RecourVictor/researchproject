f7474d09c305542de59b64a64e396d5422ed4d6d
RecourVictor <victor.recour@student.howest.be>
2025-01-08 16:32:19 +0100

    fix: cors error

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [`http://${process.env.URL_FRONTEND}`],
+    credentials: true,
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 
   console.info(`Server is running on: ${await app.getUrl()}`)
