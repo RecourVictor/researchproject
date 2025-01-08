dfdad45daa0773500bc5be89faa4e39d496fff0c
RecourVictor <victor.recour@student.howest.be>
2025-01-08 14:00:29 +0100

    feat: vue install

---
+++ packages/pwa/src/main.ts
@@ -0,0 +1,9 @@
+import { createApp, type App as VueApp } from 'vue'
+import router from './router'
+import App from './App.vue'
+
+const app: VueApp = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
