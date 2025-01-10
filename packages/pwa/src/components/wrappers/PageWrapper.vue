8a1edfc8202fa84481dd807333270aebce623059
RecourVictor <victor.recour@student.howest.be>
2025-01-10 14:32:33 +0100

    feat: create rooter

---
+++ packages/pwa/src/components/wrappers/PageWrapper.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex flex-grow items-center justify-center mx-4">
+      <div
+        class="bg-white @dark:bg-secondary-650 border-rounded-xl p-6 sm:p-8 drop-shadow-xl max-w-screen-sm w-full"
+      >
+        <RouterView />
+      </div>
+    </div>
+  </template>
