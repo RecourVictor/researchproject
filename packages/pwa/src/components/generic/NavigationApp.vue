b339049c4f05467e879206a85343fd39edc1b896
RecourVictor <victor.recour@student.howest.be>
2025-01-11 09:31:14 +0100

    feat: navigation

--- packages/pwa/src/components/generic/NavigationApp.vue
+++ packages/pwa/src/components/generic/NavigationApp.vue
@@ -1,22 +1,31 @@
 <template>
-  <nav class=" text-white p-4 flex justify-between">
+  <nav class="p-4 flex justify-between mb-16">
     <div class="flex items-center gap-6">
-        <img class="h-20" src="/logo.svg" alt="Logo Simurace" />
-        <p class="text-5xl text-wa-blue font-worldAthleticsHeading">Simurace</p>
+      <img class="h-20" src="/logo.svg" alt="Logo Simurace" />
+      <p class="text-5xl text-wa-blue font-worldAthleticsHeading">Simurace</p>
     </div>
-    <div>
-      <router-link
-        :to="{ name: 'athletes' }"
-        class=""
-        active-class="bg-red"
-      >
-        Atleten
-      </router-link>
+    <div class="flex items-center gap-6">
+      <NavigationButton :to="{ name: 'simulations' }" label="Simulaties">
+        <template #icon>
+          <Play class="text-wa-orange" />
+        </template>
+      </NavigationButton>
+      <NavigationButton :to="{ name: 'athletes' }" label="Atleten">
+        <template #icon>
+          <UserRound class="text-wa-orange" />
+        </template>
+      </NavigationButton>
+      <NavigationButton :to="{ name: 'settings' }" label="Instellingen">
+        <template #icon>
+          <Settings class="text-wa-orange" />
+        </template>
+      </NavigationButton>
     </div>
   </nav>
-  <div class="font-worldAthletics">Tekst met WorldAthletics</div>
-<h1 class="font-worldAthleticsHeading">Kop met WorldAthleticsHeading</h1>
-
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Settings, UserRound, Play } from 'lucide-vue-next'
+
+import NavigationButton from '@/components/generic/NavigationButton.vue'
+</script>
---
+++ packages/pwa/src/components/generic/NavigationButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-link
+    :to="to"
+    class="rounded-full border text-xl border-wa-blue flex items-center gap-2 py-4 px-5 transition-all duration-200 hover:bg-wa-blue hover:text-wa-orange"
+  >
+    <span class="text-xl flex items-center">
+      <slot name="icon" />
+    </span>
+    <span>{{ props.label }}</span>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  to: {
+    type: Object,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+})
+</script>
