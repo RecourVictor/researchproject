9ac0db99d94bda4813c626024c8b773f2ca6db7b
RecourVictor <victor.recour@student.howest.be>
2025-01-15 09:14:42 +0100

    feat: show dynamic athletes

--- packages/pwa/src/views/SimulationView.vue
+++ packages/pwa/src/views/SimulationView.vue
@@ -1,24 +1,63 @@
 <template>
-  <button class="z-1000" @click="togglePause">
-    {{ isPaused ? 'Hervatten' : 'Pauzeren' }}
-  </button>
-  <AthleticsTrack :athletes="athletes" :rounds="2" :isPaused="isPaused" />
+  <main v-if="loading">
+    <p>Loading...</p>
+  </main>
+
+  <main v-else>
+    <button class="z-1000" @click="togglePause">
+      {{ isPaused ? 'Hervatten' : 'Pauzeren' }}
+    </button>
+    <AthleticsTrack :athletes="athletes" :rounds="2" :isPaused="isPaused" />
+  </main>
 </template>
 
 <script setup lang="ts">
 import AthleticsTrack from '@/components/model/AthleticsTrack.vue'
 import { ref } from 'vue'
+import { GET_SIMULATION_BY_ID } from '@/graphql/simulations.query'
+import { useQuery } from '@vue/apollo-composable'
+import { useRoute } from 'vue-router'
 
 // Reactieve boolean voor pauzeren/hervatten
-const isPaused = ref(false)
+const isPaused = ref(true)
 
 // Functie om pauzeren/hervatten te schakelen
 const togglePause = () => {
   isPaused.value = !isPaused.value
 }
 
-const athletes = [
-  { id: 1, name: 'Usain Bolt', roundTime: 30.55 },
-  { id: 2, name: 'Mo Farah', roundTime: 50.30 },
-]
+// const athletes = [
+//   { id: 1, roundTime: 30.55 },
+//   { id: 2, roundTime: 50.30 },
+// ]
+const athletes = ref([])
+
+const route = useRoute()
+
+const simulationId = route.params.slug
+const {
+  result: simulationResult,
+  onResult: onSimulationResult,
+  loading,
+} = useQuery(GET_SIMULATION_BY_ID, {
+  id: String(simulationId),
+})
+
+onSimulationResult(() => {
+  if (simulationResult.value && simulationResult.value.simulation) {
+    console.log(simulationResult.value.simulation)
+
+    const rounds = simulationResult.value.simulation.disipline.rounds
+    console.log('Rounds:', rounds)
+
+    athletes.value = simulationResult.value.simulation.athletes.map(
+      (athlete: { athleteId: string; time: number }) => ({
+        id: athlete.athleteId,
+        roundTime: athlete.time / rounds,
+      }),
+    )
+    console.log('Updated athletes:', athletes.value)
+
+  }
+})
 </script>
