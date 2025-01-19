e9fb501cba5fcbb76d1643e274a8df1847a6eb2f
RecourVictor <victor.recour@student.howest.be>
2025-01-19 13:38:41 +0100

    feat: unimport unnessisary import

--- packages/pwa/src/components/generic/PopupView.vue
+++ packages/pwa/src/components/generic/PopupView.vue
@@ -23,7 +23,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineEmits } from 'vue'
+import { ref } from 'vue'
 import Heading from '@/components/generic/AppHeading.vue'
 import { Trash, X } from 'lucide-vue-next'
 import SecondaryButton from './SecondaryButton.vue'
