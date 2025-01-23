8b50d80704142203ad95bfdb0897c635e0efbd47
RecourVictor <victor.recour@student.howest.be>
2025-01-23 14:29:48 +0100

    feat: bug in env file

--- packages/pwa/src/composables/useGraphql.ts
+++ packages/pwa/src/composables/useGraphql.ts
@@ -5,6 +5,7 @@ import {
 } from '@apollo/client/core'
 
 const httpLink = createHttpLink({
+  // uri: 'http://[::1]:3000/graphql', // will fix this later with environment variables
   uri: `${import.meta.env.VITE_BACKEND_URL}/graphql`,
 })
 
