89ab38d06a0e83fac1a4d0a78658704159f905f3
RecourVictor <91408671+RecourVictor@users.noreply.github.com>
2025-03-04 16:40:25 +0100

    Update main.ts

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();  // Dit maakt een express server aan die Vercel zal gebruiken
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.enableCors({
     origin: [`http://${process.env.URL_FRONTEND}`, `http://localhost:${process.env.PWA_PORT}`],
