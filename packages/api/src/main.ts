093043bbff4b7b1084a7745e5bb6b272300a4929
RecourVictor <victor.recour@student.howest.be>
2025-03-08 14:55:33 +0100

    feat: update for feedback

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,22 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-const server = express();  // Dit maakt een express server aan die Vercel zal gebruiken
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: [`http://${process.env.URL_FRONTEND}`, `http://localhost:${process.env.PWA_PORT}`],
     credentials: true,
   })
 
-  // Voor development
-  // await app.listen(process.env.PORT ?? 3000);
-  // Deployment via vercen
-  await app.init(); // <--- Gebruik init() ipv listen()
+  await app.listen(process.env.PORT ?? 3000);
   
   console.info(`Server is running on: ${await app.getUrl()}`)
   console.info(`GraphQL is running on: ${await app.getUrl()}/graphql`)
--- packages/pwa/vite.config.ts
+++ packages/pwa/vite.config.ts
@@ -64,7 +64,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-    build: {
-    outDir: 'dist'
-  }
 })
